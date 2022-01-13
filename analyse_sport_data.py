840f30b01b2e55efe85dc7e012f81fac172e89ec
BurhanOlmez <burhan.olmezz@gmail.com>
2022-01-13 23:43:51 +0300

    Some plots are added.

--- analyse_sport_data.py
+++ analyse_sport_data.py
@@ -1,12 +1,35 @@
+from ctypes import sizeof
 from access_token_from_strava import access_token_from_strava
 from parse_running_activity_strava import parse_running_activity_strava
+import matplotlib.pyplot as plt
+
 print("Analysis for the sports data is starting!!!")
 
 if __name__ == "__main__":
     print("Analyzing...")
     access_token_from_strava_ = access_token_from_strava()
     access_token_from_strava_.run()
-    parse_running_activity_strava_ = parse_running_activity_strava(access_token_from_strava_.activities_data[0])
-    print(parse_running_activity_strava_.name)
-    print(parse_running_activity_strava_.distance)
-    print(parse_running_activity_strava_.moving_time)
+    name = []
+    distance = []
+    moving_time = []
+    start_date = []
+    average_speed = []
+    average_heartrate = []
+    for i in range(30):
+        print(i)
+        if access_token_from_strava_.activities_data[i]["type"] == "Run":
+            parse_running_activity_strava_ = parse_running_activity_strava(access_token_from_strava_.activities_data[i])
+            name.append(parse_running_activity_strava_.name) 
+            distance.append(parse_running_activity_strava_.distance) 
+            moving_time.append(parse_running_activity_strava_.moving_time) 
+            start_date.append(parse_running_activity_strava_.start_date) 
+            average_speed.append(parse_running_activity_strava_.average_speed) 
+            average_heartrate.append(parse_running_activity_strava_.average_heartrate) 
+    
+    fig, axs = plt.subplots(3)
+    fig.suptitle('Running Analysis')
+    axs[0].plot(range(len(name)), distance)
+    axs[1].plot(range(len(name)), average_speed)
+    axs[2].plot(range(len(name)), average_heartrate)
+    plt.show()
+    
